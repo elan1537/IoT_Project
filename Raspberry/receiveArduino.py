5bf6397bb5e4debe5a282e4c5fe8acb0af3c7de6
Pengpark <qkrtpgus2280>
2016-03-01 10:30:39 +0900

    아두이노로부터 데이터 송신

---
+++ Raspberry/receiveArduino.py
@@ -0,0 +1,44 @@
+import time
+
+import RPi.GPIO as GPIO
+import spidev
+
+from lib_nrf24 import NRF24
+
+GPIO.setmode(GPIO.BCM)
+
+pipes = [[0xE8, 0xE8, 0xF0, 0xF0, 0xE1], [0xF0, 0xF0, 0xF0, 0xF0, 0xE1]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 17)
+
+radio.setPayloadSize(32)
+radio.setChannel(0x76)
+radio.setDataRate(NRF24.BR_1MBPS)
+radio.setPALevel(NRF24.PA_MIN)
+
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+radio.openReadingPipe(1, pipes[1])
+radio.printDetails()
+radio.startListening()
+
+while True:
+
+    while not radio.available(0):
+        time.sleep(0.01)
+
+    receivedMessage = []
+    radio.read(receivedMessage, radio.getDynamicPayloadSize())
+    print("Received: {}".format(receivedMessage))
+
+    print("Translating our received Messageinto unicode characters....")
+    string = ""
+
+    for n in receivedMessage:
+        if (n >= 32 and n <= 126):
+            string += chr(n)
+    print("Our received message decodes to: {}".format(string))
+    
